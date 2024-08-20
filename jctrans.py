72cf3b9f1ae15169e435cdca575aa90d43668b23
Wing Yang <wanggui.yang@sunmi.com>
2024-08-20 15:22:15 +0800

    fix:

--- jctrans.py
+++ jctrans.py
@@ -4,24 +4,11 @@ import time
 from datetime import datetime
 
 import requests
+from openpyxl.reader.excel import load_workbook
 from openpyxl.workbook import Workbook
 from playwright.async_api import async_playwright
-
-
-def create_excel(data, filename):
-    # 创建一个新的 Excel 工作簿
-    wb = Workbook()
-    ws = wb.active
-    # 将数据写入工作表
-    for row in data:
-        ws.append(row)
-    # 保存 Excel 文件
-    directory = 'data'
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    filename = os.path.join(directory, filename)
-    wb.save(filename)
-    print("创建成功：", filename)
+from pyppeteer import launch
+import asyncio
 
 
 def post(url, data):
@@ -36,93 +23,56 @@ def post(url, data):
 
 
 # 获取所有的公司uid
-def get_all_uid(country_id):
-    url = 'https://cloudapi.jctrans.com/era/fr/shop/companyDirectory'
-    data = {
-        "current": 1,
-        "size": 100000,
-        "advCodeList": [],
-        "vipCodeList": [],
-        "minVipTotalYears": 0,
-        "maxVipTotalYears": 150,
-        "countryId": country_id
-    }
-    response = post(url, data)
-    if response.status_code == 200:
-        uid_list = []
-        response_data = response.json()
-        if 'data' in response_data:
-            total = response_data['data']['total']
-            data_records = response_data['data']['records']
-            if data_records:
-                for record in data_records:
-                    if 'uid' in record:
-                        uid = record['uid']
-                        uid_list.append(uid)
-            print(f"country_id:{country_id},total:{total}")
-            return uid_list, total
+def get_all_uid():
+    country_id = 7
+
+    for i in range(1, 10):
+        print("i", i)
+        url = 'https://cloudapi.jctrans.com/era/fr/shop/companyDirectory'
+        data = {
+            "current": i,
+            "size": 1000,
+            "advCodeList": [],
+            "vipCodeList": [],
+            "minVipTotalYears": 0,
+            "maxVipTotalYears": 150,
+            "countryId": country_id
+        }
+        response = post(url, data)
+        if response.status_code == 200:
+            uid_list = []
+            country_name = ""
+            response_data = response.json()
+            if 'data' in response_data:
+                total = response_data['data']['total']
+                if total == 0: # 结束
+                    return
+                data_records = response_data['data']['records']
+                if data_records:
+                    for record in data_records:
+                        if 'uid' in record:
+                            uid = record['uid']
+                            uid_list.append(uid)
+                        if 'countryName' in record and country_name == "":
+                            country_name = record['countryName']
+                print(
+                    f"countryName:{country_name},country_id:{country_id},total:{total},len:{len(response_data['data']['records'])}")
+                with open(f"./country/{country_name}_{country_id}_{total}.txt", 'a') as file:
+                    for uid in uid_list:
+                        file.write(uid + '\n')
+                # return country_name, country_id, uid_list, total
+            else:
+                print('未找到 uid 字段')
         else:
-            print('未找到 uid 字段')
-    else:
-        print('POST 请求失败')
+            print('POST 请求失败')
+
+    return None, None, None, None
 
 
-async def get_index_context(page, url):
-    company = ""
-    email = ""
-    phone = ""
-    await page.goto(url)
-    await page.wait_for_load_state('networkidle')
-
-    h1_elements = await page.locator('xpath=//h1').all()
-    if len(h1_elements) >= 2:
-        company = await h1_elements[2].inner_text()
-
-    content_elements = await page.locator('.content').all()
-    if len(content_elements) >= 3:
-        email = await content_elements[-3].inner_text()
-    if len(content_elements) >= 2:
-        phone = await content_elements[-2].inner_text()
-    if "@" in email:
-        pass
-    else:
-        email = phone
-        phone = ""
-    return company, email, phone
-
-
-async def main():
-    cookie_value = "b1a5bfe5040b4ecea0860368c4b321e2"
-    country_id = 67
-    uid_list, total = get_all_uid(country_id)
-    index_data_list = [[]]
-
-    async with async_playwright() as p:
-        browser = await p.chromium.launch()
-        context = await browser.new_context()
-        page = await context.new_page()
-        await context.add_cookies([{
-            "domain": ".jctrans.com",
-            "name": "JC-JAVA-Token",
-            "path": "/",
-            "value": cookie_value
-        }])
-
-        count = 0
-        for uid in uid_list:
-            url = 'https://www.jctrans.com/cn/home/' + uid
-            company, email, phone = await get_index_context(page, url)
-            count += 1
-            print(count, company, email, phone)
-            index_data_list.append([company, email, phone])
-        await browser.close()
-
-    current_time = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H-%M-%S')
-    filename = f"巴基斯坦_{country_id}_{total}_{current_time}.xlsx"
-    create_excel(index_data_list, filename)
+def main():
+    get_all_uid()
+    return
 
 
 if __name__ == '__main__':
-    import asyncio
-
-    asyncio.run(main())
+    main()
---
+++ run7.py
@@ -0,0 +1,129 @@
+import json
+import os
+import time
+from datetime import datetime
+
+import requests
+from openpyxl.reader.excel import load_workbook
+from openpyxl.workbook import Workbook
+from playwright.async_api import async_playwright
+from pyppeteer import launch
+import asyncio
+
+
+def append_to_excel(data, filename):
+    directory = 'data'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    filename = os.path.join(directory, filename)
+    if os.path.exists(filename):
+        wb = load_workbook(filename)
+        ws = wb.active
+    else:
+        wb = Workbook()
+        ws = wb.active
+    # for row in data:
+    ws.append(data)
+
+    wb.save(filename)
+
+
+def post(url, data):
+    json_data = json.dumps(data)
+    headers = {
+        'Content-Type': 'application/json',
+        'Cookie': 'HWWAFSESID=090d6dcbb843b5c5e6; HWWAFSESTIME=1723626452297'
+    }
+    # 发送POST请求
+    response = requests.post(url, headers=headers, data=json_data)
+    return response
+
+
+# 获取所有的公司uid
+def get_all_uid():
+    country_id = 7
+    uid_list = []
+    country_name = "Egypt"
+    total = 939
+
+    # 读取文件
+    workbook_path = f"./data/{country_name}_{country_id}_{total}.xlsx"
+    if os.path.exists(workbook_path):
+        workbook = load_workbook(workbook_path)
+        sheet = workbook.active
+        start_line_number = sheet.max_row + 1
+    else:
+        start_line_number = 1
+    with open(f"./country/{country_name}_{country_id}_{total}.txt", 'r') as file:
+        lines = file.readlines()
+        for line_number, line in enumerate(lines, start=1):
+            if line_number < start_line_number:
+                continue
+            uid_list.append(line.strip())
+    return country_name, country_id, uid_list, total
+
+
+async def get_index_context(page, url):
+    company = ""
+    email = ""
+    phone = ""
+    await page.goto(url)
+    await page.wait_for_load_state('networkidle')
+
+    h1_elements = await page.locator('xpath=//h1').all()
+    if len(h1_elements) >= 2:
+        company = await h1_elements[2].inner_text()
+
+    content_elements = await page.locator('.content').all()
+    if len(content_elements) >= 3:
+        email = await content_elements[-3].inner_text()
+    if len(content_elements) >= 2:
+        phone = await content_elements[-2].inner_text()
+    if "@" in email:
+        pass
+    else:
+        email = phone
+        phone = ""
+    return company, email, phone
+
+
+async def fetch_data_with_retry(page, url, retry=10):
+    for _ in range(retry):
+        try:
+            company, email, phone = await get_index_context(page, url)
+            return company, email, phone
+        except Exception as e:
+            print(f"Failed to fetch data from {url}. Retrying...")
+    return None, None, None
+
+
+async def main():
+    cookie_value = "19ca1ae55aab4903806925a7d71ae4aa"
+    country_name, country_id, uid_list, total = get_all_uid()
+
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        context = await browser.new_context()
+        page = await context.new_page()
+        await context.add_cookies([{
+            "domain": ".jctrans.com",
+            "name": "JC-JAVA-Token",
+            "path": "/",
+            "value": cookie_value
+        }])
+
+        count = 0
+        for uid in uid_list:
+            url = 'https://www.jctrans.com/cn/home/' + uid
+            company, email, phone = await fetch_data_with_retry(page, url)
+            count += 1
+            print(count, company, email, phone)
+            filename = f"{country_name}_{country_id}_{total}.xlsx"
+            append_to_excel([company, email, phone], filename)
+        await browser.close()
+
+
+if __name__ == '__main__':
+    import asyncio
+
+    asyncio.run(main())
